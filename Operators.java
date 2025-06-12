eb78664cba57baa02948c9e5a4f3f82c1e383899
kommemohan <138978318+kommemohan@users.noreply.github.com>
2025-06-12 19:58:02 +0530

    Update Operators.java
    adding relational operators

--- Operators.java
+++ Operators.java
@@ -10,13 +10,62 @@ public class Operators {
         int add = firstNumber + sencondNumber;
 
         System.out.println("Addition : " +add);
+
+        Scanner readNumbe = new Scanner(System.in);
+        System.out.print(" Enter a number : ");
+
+        int number = readNumbe.nextInt();
+
+        // check a number divisible by both 2 and 5 ;   logical AND  &&
+
+        if( (number % 2 == 0)  && (number % 5 == 0 ) ){
+            System.out.println("Divisible by both 2 and 5");
+
+        }
+        else {
+            System.out.println("invalid number");
+        }
+
+
+
+
+
+
+
+
+        // check a number divisible by either 2 or 5 ;  logical OR  ||
+
+        if( ( number % 2 == 0 ) || ( number % 5 ==0)) {
+            System.out.println(" Divisible by either 2 or 5 ");
+
+        }
+        else{
+            System.out.println(" Invalid Number");
+        }
+
+
+
+
+
+
+
+
+        // check a number not divisible by 2
+
+        if ( !(number % 2 ==0) ){
+            System.out.println(" divisible by 2 ");
+
+        }
+        else {
+            System.out.println("invalid numbmer ");
+        }
+
+
+
+        System.out.println(10 & 15); // 10
+        System.out.println( 10 | 15); // 15
+        
+        
+        
         }
 }
-
-output :
-
-Enter a number : 
-5
- Enter Second Number : 
-10
-Addition : 15
