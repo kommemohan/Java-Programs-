e0603914b8e8efa2568f374293f32d1af70a2af4
kommemohan <138978318+kommemohan@users.noreply.github.com>
2025-06-13 14:22:58 +0530

    Create RelationalOperater.java
    i am uploading relational operator program

---
+++ RelationalOperater.java
@@ -0,0 +1,30 @@
+package allJavaProblems;
+
+public class RelationalOperators {
+    public static void main(String []args) {
+
+        int a = 10;
+        int b = 50;
+        System.out.println(a < b);  // true
+        System.out.println(a > b);    //false
+        System.out.println(a <= b); //true
+        System.out.println(a >=b );    // false
+        System.out.println(a ==b );      //false
+        System.out.println(a !=b );   //true
+
+        add(10,50); // call by value
+
+
+
+
+    }
+
+    private static void add(int a, int b) {
+        int sum = a+b; // sum = 60
+        System.out.println(sum);
+    }
+}
+
+
+
+
